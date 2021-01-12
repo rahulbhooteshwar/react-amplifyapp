427ec276704f9aac3de645834dcd99809e9f49c6
Rahul Bhooteshwar <Rahul.Bhooteshwar@gmail.com>
2021-01-12 11:02:24 +0530

    feat(feature-1): initail version of feature1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello From Dev</h1>
+        <div style={{backgroundColor: 'yellow', color:'orange'}}>
+          <h2>Feature1</h2>
+        </div>
       </header>
     </div>
   );
