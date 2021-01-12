e368820af352bdaeb1a73d1ba398eb3ace182aa7
Rahul Bhooteshwar <Rahul.Bhooteshwar@gmail.com>
2021-01-12 10:46:13 +0530

    start dev

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello From v2</h1>
+        <h1>Hello From Dev</h1>
       </header>
     </div>
   );
